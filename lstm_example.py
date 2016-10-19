1b9db95c48359293c848d65a485f702efa35f7be
Minh Nguyen <minh.nguyen@smail.inf.h-brs.de>
2016-10-19 11:16:08 +0200

    add model weights log, change naming scheme

--- lstm_example.py
+++ lstm_example.py
@@ -65,5 +65,5 @@ model_json = model.to_json()
 with open("model.json", "w") as json_file:
     json_file.write(model_json)
 # serialize weights to HDF5
-model.save_weights("model.h5")
+model.save_weights("weights.h5")
 print("Saved model to disk")

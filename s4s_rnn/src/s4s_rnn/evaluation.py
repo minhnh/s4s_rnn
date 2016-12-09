25b943bd738a1ddf1eea7e50b7d16445d28c7cca
Minh Nguyen <minhnh.91@gmail.com>
2016-12-09 16:44:05 +0100

    add class for prediction results

---
+++ s4s_rnn/src/s4s_rnn/evaluation.py
@@ -0,0 +1,8 @@
+class ExperimentPredictions(object):
+    def __init__(self, exp_name, subject_name):
+        self.experiment_name = exp_name
+        self.subject_name = subject_name
+        self.predictions = {}
+        pass
+
+    pass

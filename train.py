cb6db41dfc243b54e84f8d6a3b5df9cae2c17e62
Minh Nguyen <minhnh.91@gmail.com>
2016-12-13 22:45:01 +0100

    refactor cross validation function

--- train.py
+++ train.py
@@ -17,22 +17,83 @@ def get_arguments():
                         choices=['lstm', 'gru'], default='lstm',
                         help='RNN model to train')
     parser.add_argument('--scenarios', '-s', type=str,
-                        choices=['cross_val', 'vary_num_neuron'], default='cross_val',
+                        choices=['cross_val', 'vary_hidden'],
+                        default='cross_val',
                         help='training scenarios to run')
     return parser.parse_args()
 
 
-def main(arguments):
-    import numpy as np
+def train_cross_validation(sessions, model, num_hidden, num_tsteps,
+                           input_dim, output_dim, date_string):
     from sklearn.model_selection import KFold
     from keras.models import model_from_json
-    from keras.callbacks import CSVLogger, ReduceLROnPlateau
+    from keras.callbacks import CSVLogger
+
+    from s4s_rnn import models, utils
+
+    model = models.create_model(model, num_hidden, input_dim=None,
+                                input_shape=(num_tsteps, input_dim),
+                                output_dim=output_dim)
+    # Construct meaningful base name
+    base_name = "%s_indoor_%s_%02dstep_%02din_%03dhidden_%03depoch_" \
+                % (arguments.model, date_string, num_tsteps, input_dim,
+                   num_hidden, arguments.num_epoch)
+    base_name = os.path.join("train_results", base_name)
+
+    # serialize model to JSON
+    model_json = model.to_json()
+    model_file_name = base_name + "model.json"
+    with open(model_file_name, "w") as json_file:
+        json_file.write(model_json)
+        pass
+
+    print("\n----------------------------------------------------\n")
+    print("Base file name: %s" % (base_name))
+    print("Looking back %d time steps\n" % (num_tsteps))
+
+    kf = KFold(len(sessions))
+    for train_index, test_index in kf.split(sessions):
+        print("\n--------------------------\n")
+        train_sessions = sessions[train_index]
+        test_sessions = sessions[test_index]
+        print("Training on:\n" + "\n".join(map(str, train_sessions)))
+        print("\nTesting on:\n" + "\n".join(map(str, test_sessions)))
+
+        print("\nLoading model from: " + model_file_name)
+        json_file = open(model_file_name, 'r')
+        loaded_model_json = json_file.read()
+        json_file.close()
+        loaded_model = model_from_json(loaded_model_json)
+        loaded_model.compile(loss='mean_squared_error', optimizer='rmsprop')
+
+        match = re.match('.+/running_indoor_(.+)/(\d+)>', str(test_sessions[0]))
+        # put name of evaluation set in saved filenames
+        cross_validation_name = base_name + match.groups()[0] + "_" + match.groups()[1] + "_"
+
+        train_data_x, train_data_y = utils.get_data_from_sessions(train_sessions, num_tsteps)
+        test_data_x, test_data_y = utils.get_data_from_sessions(test_sessions, num_tsteps)
+
+        csv_logger = CSVLogger(cross_validation_name + "training.log", append=False)
+        # reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.9, patience=10, min_lr=0.001)
+        loaded_model.fit(train_data_x, train_data_y, batch_size=(1),
+                         nb_epoch=arguments.num_epoch, validation_data=(test_data_x, test_data_y),
+                         callbacks=[csv_logger], verbose=2)
+
+        # serialize weights to HDF5
+        loaded_model.save_weights(cross_validation_name + "weights.h5")
+        print("Saved model to disk")
+
+        pass
+
+    return
+
+
+def main(arguments):
+    import numpy as np
 
     from sweat4science.workspace.Workspace import Workspace
     from sweat4science.evaluation.sessionset import MF_sessionset as mfs
     from sweat4science import s4sconfig
-
-    from s4s_rnn import models, utils
 
     workspace_folder = os.path.join(s4sconfig.workspace_dir, "session-data")
     ws = Workspace(workspace_folder)
@@ -55,60 +116,9 @@ def main(arguments):
     hidden_neurons = 400
     date_string = time.strftime("%Y%m%d")
 
-    # Cross validation training
-    kf = KFold(len(sessions))
-
     for ntsteps in [5, 10, 15]:
-        model = models.create_model(arguments.model, hidden_neurons, input_dim=None,
-                                    input_shape=(ntsteps, input_dim),
-                                    output_dim=output_dim)
-        # Construct meaningful base name
-        base_name = "%s_indoor_%s_%02dstep_%02din_%03dhidden_%03depoch_" \
-                    % (arguments.model, date_string, ntsteps, input_dim,
-                       hidden_neurons, arguments.num_epoch)
-        base_name = os.path.join("train_results", base_name)
-
-        # serialize model to JSON
-        model_json = model.to_json()
-        model_file_name = base_name + "model.json"
-        with open(model_file_name, "w") as json_file:
-            json_file.write(model_json)
-            pass
-        print("\n----------------------------------------------------\n")
-        print("Base file name: %s" % (base_name))
-        print("Looking back %d time steps\n" % (ntsteps))
-        for train_index, test_index in kf.split(sessions):
-            print("\n--------------------------\n")
-            train_sessions = sessions[train_index]
-            test_sessions = sessions[test_index]
-            print("Training on:\n" + "\n".join(map(str, train_sessions)))
-            print("\nTesting on:\n" + "\n".join(map(str, test_sessions)))
-
-            print("\nLoading model from: " + model_file_name)
-            json_file = open(model_file_name, 'r')
-            loaded_model_json = json_file.read()
-            json_file.close()
-            loaded_model = model_from_json(loaded_model_json)
-            loaded_model.compile(loss='mean_squared_error', optimizer='rmsprop')
-
-            match = re.match('.+/running_indoor_(.+)/(\d+)>', str(test_sessions[0]))
-            # put name of evaluation set in saved filenames
-            cross_validation_name = base_name + match.groups()[0] + "_" + match.groups()[1] + "_"
-
-            train_data_x, train_data_y = utils.get_data_from_sessions(train_sessions, ntsteps)
-            test_data_x, test_data_y = utils.get_data_from_sessions(test_sessions, ntsteps)
-
-            csv_logger = CSVLogger(cross_validation_name + "training.log", append=False)
-            #reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.9, patience=10, min_lr=0.001)
-            loaded_model.fit(train_data_x, train_data_y, batch_size=(1),
-                             nb_epoch=arguments.num_epoch, validation_data=(test_data_x, test_data_y),
-                             callbacks=[csv_logger], verbose=2)
-
-            # serialize weights to HDF5
-            loaded_model.save_weights(cross_validation_name + "weights.h5")
-            print("Saved model to disk")
-
-            pass
+        train_cross_validation(sessions, arguments.model, hidden_neurons, ntsteps,
+                           input_dim, output_dim, date_string)
         pass
     return
 

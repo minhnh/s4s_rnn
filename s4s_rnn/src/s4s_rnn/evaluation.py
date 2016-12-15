0e9a9fe427f5f4c8d5182e9d61461ed6a87333ae
Minh Nguyen <minhnh.91@gmail.com>
2016-12-15 01:35:46 +0100

    add class to store predictions by sessions

--- s4s_rnn/src/s4s_rnn/evaluation.py
+++ s4s_rnn/src/s4s_rnn/evaluation.py
@@ -1,6 +1,9 @@
+import os
+import re
 import numpy as np
-from s4s_rnn import utils
 from sklearn.preprocessing import MinMaxScaler
+
+from s4s_rnn import utils
 
 
 _MINMAX = MinMaxScaler.__name__
@@ -63,8 +66,7 @@ class ExperimentEvalutation(object):
             self.predictions[name] = prediction
             pass
 
-        self.mse[name] = np.mean((self.predictions[name]
-                                  - self.true_output[-len(prediction):])**2)
+        self.mse[name] = np.mean((self.predictions[name] - self.true_output[-len(prediction):])**2)
         return
 
     def plot_predictions(self, prediction_names, plot_tiltle, file_name=None):
@@ -77,6 +79,71 @@ class ExperimentEvalutation(object):
         predictions = list(map(self.predictions.get, prediction_names))
         utils.plot_predictions(predictions, prediction_names, self.true_output,
                                plot_tiltle, file_name=file_name)
+        return
+
+    pass
+
+
+class ExperimentEvalutationDict(dict):
+
+    def __init__(self, evaluations=None):
+        if evaluations is None:
+            super().__init__()
+            return
+
+        super().__init__(evaluations)
+        self.model_json = {}
+        for key, value in iter(evaluations):
+            if value.__class__.__name__ != ExperimentEvalutation.__name__:
+                raise ValueError("%s: Expected %s, got: %s" % (self.__class__.__name__,
+                                                               ExperimentEvalutation.__name__,
+                                                               value.__class__.__name__))
+                pass
+
+            if key != value.experiment_name:
+                raise ValueError("%s: key %s does not match name %s"
+                                 % (self.__class__.__name__, key, value.experiment_name))
+                pass
+
+            self.update_experiment(value)
+
+            pass
+        return
+
+    def update_experiment(self, exp_eval):
+        #TODO: check prediction_name in self.model_json
+        return
+
+    def add_model_json(self, model_file_name, update=False):
+        base_name = os.path.basename(model_file_name)
+        match = re.match('(gru|lstm)_\w+_(\d{8})_(\d{2})step_(\d{2})in_(\d{3})hidden_(\d{3})epoch_model.json',
+                         base_name)
+        if match is None:
+            print("%s: Invalid model file name: %s" % (self.__class__.__name__, base_name))
+            return
+
+        model_type = match.group(1)
+        date_string = match.group(2)
+        num_tstep = match.group(3)
+        num_input = match.group(4)
+        num_neuron = match.group(5)
+        num_epoch = match.group(6)
+        # print("%s_%s_%s_%s_%s_%s" % (model_type, date_string, num_tstep, num_input, num_neuron, num_epoch))
+        prediction_name = "%s_lookback%s_%sneurons" % (model_type, num_tstep, num_neuron)
+
+        if prediction_name in self.model_json\
+                and self.model_json[prediction_name] is not None\
+                and not update:
+            return
+
+        json_file = open(model_file_name, 'r')
+        loaded_model_json = json_file.read()
+        json_file.close()
+
+        self.model_json[prediction_name] = loaded_model_json
+        return
+
+    def add_experiment_from_file(self, file_name):
         return
 
     pass
